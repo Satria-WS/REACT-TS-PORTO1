c3205272d9a660c249c1a289405b5ddbbecf69f7
Satria-WS <extencezone02@gmail.com>
2023-05-03 01:08:44 +0700

    update navbar ts

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
 import "./about.css";
 
 const About = () => {
-  return (
-    <section id='about'>about</section>
-  )
-}
+  return <section id="about">about</section>;
+};
 
-export default About
+export default About;
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,27 +1,63 @@
-
-import React from 'react'
+import React from "react";
 
 import "./nav.css";
-import {AiOutlineHome , AiOutlineUser} from "react-icons/ai"
-import {BiBook, BiMessageSquareDetail} from "react-icons/bi";
-import {RiServiceLine} from "react-icons/ri";
-import {useState , FC} from 'react';
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { useState, FC } from "react";
 
-
-const Nav : React.FunctionComponent = ()  => {
-  const [activeNav , setActiveNav] = useState('#')
-
-
+const Nav: React.FunctionComponent = () => {
+  const [activeNav, setActiveNav] = useState("#");
 
   return (
     <nav>
-      <a href="#" className='active'><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BiMessageSquareDetail/></a>
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+          setActiveNav("#")
+        }
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        className={activeNav === "#about" ? "active" : ""}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+          setActiveNav("#about")
+        }
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        className={activeNav === "#experience" ? "active" : ""}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+          setActiveNav("#experience")
+        }
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        className={activeNav === "#services" ? "active" : ""}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+          setActiveNav("#services")
+        }
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        className={activeNav === "#contact" ? "active" : ""}
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+          setActiveNav("#contact")
+        }
+      >
+        <BiMessageSquareDetail />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
