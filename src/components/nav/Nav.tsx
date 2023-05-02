a9ece1d075b42f8d27c10334b610237081ca9fec
satria wibowo <extencezone02@gmail.com>
2023-05-02 17:29:35 +0700

    1:19:10

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -5,12 +5,17 @@ import "./nav.css";
 import {AiOutlineHome , AiOutlineUser} from "react-icons/ai"
 import {BiBook, BiMessageSquareDetail} from "react-icons/bi";
 import {RiServiceLine} from "react-icons/ri";
+import {useState , FC} from 'react';
 
 
-const Nav = () => {
+const Nav : React.FunctionComponent = ()  => {
+  const [activeNav , setActiveNav] = useState('#')
+
+
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
+      <a href="#" className='active'><AiOutlineHome/></a>
       <a href="#about"><AiOutlineUser/></a>
       <a href="#experience"><BiBook/></a>
       <a href="#services"><RiServiceLine/></a>
